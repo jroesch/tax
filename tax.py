1f6b7ee7b01d658142c3fc9679a62cc6b922b1e5
Jared Roesch <roeschinc@gmail.com>
2022-07-10 19:51:22 -0700

    Update to show how tax.jit works with jax.grad

--- tax.py
+++ tax.py
@@ -14,6 +14,7 @@ from jax._src.util import safe_map
 import tvm
 from tvm import relay
 from tvm import IRModule
+from jax._src.ad_util import add_jaxvals_p
 
 def example(x):
   return jnp.exp(jnp.tanh(x))
@@ -33,6 +34,13 @@ def jit(fun):
 PRIM_MAP = {
     jax.lax.tanh_p: relay.op.tanh,
     jax.lax.exp_p: relay.op.exp,
+    jax.lax.sub_p: relay.op.subtract,
+    jax.lax.mul_p: relay.op.multiply,
+    # Not sure this is actually right, but add_any seems under documented, looks like abstract addition for grad?
+    # how to eval, maybe look at HLO lowering rules?
+    # It looks like Lax defintions set add_any to be mapped to add for most types, see here:
+    # https://github.com/google/jax/blob/847c04fd8c86f7cec69625cf7e0ade1cb80a1f72/jax/_src/lax/lax.py#L1411
+    add_jaxvals_p: relay.op.add,
 }
 
 def tvm_interpret_jaxpr(jaxpr, consts, *args):
@@ -51,7 +59,15 @@ def tvm_interpret_jaxpr(jaxpr, consts, *args):
 
     for eqn in jaxpr.eqns:
         assert len(eqn.params) == 0, "params not supported"
-        inputs = [env[var] for var in eqn.invars]
+        inputs = []
+        for var in eqn.invars:
+            if isinstance(var, core.Var):
+                inputs.append(env[var])
+            elif isinstance(var, core.Literal):
+                inputs.append(relay.const(var.val))
+            else:
+                raise Exception(f"unsupported input variable type {type(var)}")
+
         output = PRIM_MAP[eqn.primitive](*inputs)
         env[eqn.outvars[0]] = output
 
@@ -71,4 +87,8 @@ def tvm_interpret_jaxpr(jaxpr, consts, *args):
     result = exec.evaluate()(*tvm_args)
     return result
 
+# Example of using tax.jit
 print(jit(example)(1.0))
+
+# Example of using tax.jit with
+print(jit(grad(example))(1.0))
